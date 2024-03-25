2167e8f07f8e1d209b9cf1b11704fbff4433066d
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-03-25 00:52:26 -0500

    Player Moving Around

---
+++ scenes/Player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed: int = 125.0
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	if direction_x:
+		velocity.x = direction_x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		velocity.y = direction_y * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+
+	move_and_slide()
