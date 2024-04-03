6f9fb3ebfcf0405f0e2aac1bb9d76dbbcf3f715e
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-04-03 10:24:43 -0500

    Added room objects

--- scenes/gamestates/CurrentRoom.gd
+++ scenes/gamestates/CurrentRoom.gd
@@ -2,27 +2,10 @@ extends Node2D
 
 @export var player: Player;
 
-@onready var tilemap: TileMap = get_child(0).get_child(0)
-
-var tile_rect: Rect2i;
-var tile_data: TileData;
-var start_found: bool = false;
+@onready var room_spawn: Marker2D = get_child(0).get_child(1).get_child(0)
 
 func init_player_pos():
-	tile_rect = tilemap.get_used_rect()
-	
-	start_found = false;
-	for x in range(tile_rect.position.x, tile_rect.end.x):
-		for y in range(tile_rect.position.y, tile_rect.end.y):
-			# print(Vector2i(x,y))
-			tile_data = tilemap.get_cell_tile_data(0, Vector2i(x, y));
-			if (tile_data != null):
-				print(tile_data.get_custom_data("interaction_type"))
-				if (tile_data.get_custom_data("interaction_type") == 1):
-					player.position = Vector2((x * tilemap.tile_set.tile_size[0]) + tilemap.tile_set.tile_size[0]/2, (y * tilemap.tile_set.tile_size[1]) + tilemap.tile_set.tile_size[1]/2)
-					start_found = true;
-					break;
-		if (start_found): break;
+	player.position = room_spawn.position;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
