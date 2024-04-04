bc92e00455423e0db1d3de7ef8bfd457ff3a2f31
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-04-03 23:56:32 -0500

    Made room load seperately

--- scenes/gamestates/CurrentRoom.gd
+++ scenes/gamestates/CurrentRoom.gd
@@ -2,18 +2,30 @@ extends Node2D
 
 @export var player: Node2D;
 
-@onready var room_spawn: Marker2D = get_child(0).get_child(1).get_child(0)
+var room: PackedScene;
+var room_scene;
+var room_spawn: Marker2D;
 
 func init_player_pos():
 	player.position = room_spawn.position;
 
+func set_room(path: String):
+	room = load(path)
+	room_scene = room.instantiate()
+	room_scene.y_sort_enabled = true;
+	add_child(room_scene)
+	get_parent().get_child(1).BoundingTarget = room_scene;
+	room_spawn = room_scene.get_child(1).get_child(0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_room("res://scenes/rooms/Puzzle Loop Room.tscn")
+	
 	init_player_pos()
 	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
