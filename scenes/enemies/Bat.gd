1db6ea4e0911d55a6f63320489d52a1845e5981d
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-04-05 01:37:53 -0500

    Added bat base

---
+++ scenes/enemies/Bat.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed: int = 50;
+
+var is_chasing: bool = false;
+var target: Node2D;
+
+func _physics_process(_delta):
+	if (is_chasing):
+		velocity = (target.get_global_position() - position).normalized() * speed
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, 0.07)
+		
+	move_and_slide()
+
+func _on_player_rect_body_entered(body):
+	is_chasing = true;
+	target = body
+	get_child(2).set_deferred("disabled", true);
+	
