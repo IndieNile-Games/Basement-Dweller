40334fcf7355d16df199a420544c51eb4882a1d7
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-04-06 21:51:53 -0500

    Revert "Make bat angary attempt"
    This reverts commit 6937a7f2d56fe8aeeddf575581b5f7e621110742.

--- scenes/gamestates/dungon/enemies/bat/Bat.gd
+++ scenes/gamestates/dungon/enemies/bat/Bat.gd
@@ -1,28 +1,12 @@
-extends BaseEnemy
+extends CharacterBody2D
 
-@export_category("Bat Config")
 @export var speed: int = 50;
 
-var invulnerable: bool = false;
-var dead: bool = false;
-
-func attack():
-	if (!dead):
-		velocity = (target.get_global_position() + position).normalized() * speed
-		health = health - 1;
-		
-		if (health <= 0):
-			dead = true;
-			emit_signal("enemy_killed")
-
-func _ready():
-	if (link_code != -1):
-		for actionable in actionables_box.get_children():
-			if (link_code == actionable.link_code):
-				connect("enemy_killed", actionable._change_state)
+var is_chasing: bool = false;
+var target: Node2D;
 
 func _physics_process(_delta):
-	if (targeting):
+	if (is_chasing):
 		velocity = (target.get_global_position() - position).normalized() * speed
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, 0.07)
@@ -30,16 +14,7 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func _on_player_rect_body_entered(body):
-	targeting = true;
+	is_chasing = true;
 	target = body
 	$"Room Collision Box".set_deferred("disabled", true);
-
-func _on_hit_box_area_entered(_weapon):
-	print("Test Bat")
-	if (!invulnerable):
-		attack()
-		$InvulTimer.start();
-		invulnerable = true;
-
-func _on_invul_timer_timeout():
-	invulnerable = false;
+	
